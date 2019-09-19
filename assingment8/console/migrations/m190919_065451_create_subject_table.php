50b9ab1ebce8d601777b2b6bd11b6c6eeed52ff4
สรยุทธ ชัชราวรรณ <s5902041620202@email.kmutnb.ac.th>
2019-09-19 15:01:40 +0700

    update

--- assingment8/console/migrations/m190919_065451_create_subject_table.php
+++ assingment8/console/migrations/m190919_065451_create_subject_table.php
@@ -5,7 +5,7 @@ use yii\db\Migration;
 /**
  * Handles the creation of table `{{%subject}}`.
  */
-class m190905_081635_create_subject_table extends Migration
+class m190919_065451_create_subject_table extends Migration
 {
     /**
      * {@inheritdoc}
@@ -14,6 +14,8 @@ class m190905_081635_create_subject_table extends Migration
     {
         $this->createTable('{{%subject}}', [
             'id' => $this->primaryKey(),
+            'name'=>$this->string(256)->notNull(),
+            'detail'=>$this->text()
         ]);
     }
 
